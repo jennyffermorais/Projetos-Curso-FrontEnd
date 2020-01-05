5120a7c971c736b19ceb13eafb2473133f8bb294
Jennyffer de Morais <jennyffernovaes@hotmail.com>
2020-01-05 12:52:56 -0300

    Adicionando projeto da unidade 8 programacao basica

---
+++ 8-Cliente-e-Servidor/index.js
@@ -0,0 +1,22 @@
+var expressjs = require("express");
+
+var aplicacao = expressjs();
+
+aplicacao.get("/", hello);
+aplicacao.get("/cursos", cursos);
+aplicacao.get("/hora", retornaHora);
+
+function retornaHora(req, res) {
+  res.send("A hora atual é: " + new Date().getHours());
+}
+
+function hello(req, res) {
+  res.send("<h1>Olá tudo bem?</h1>");
+}
+
+function cursos(req, res) {
+  res.send("<ul><li>Programação básica</li><li>Programação Avançada</li></ul>");
+}
+
+aplicacao.listen(8989);
+console.log("Aplicação em execução...");
